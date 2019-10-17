81e2d6be6f5e47f5aabaa1fef3afd45f29a589a6
IzakAlva <f.i.haak@students.uu.nl>
2019-10-17 15:51:45 +0200

    init

---
+++ Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+    import Controller
+    import Model
+    import View
+    import Datatypes
+    
+    import Graphics.Gloss.Interface.IO.Game
+    
+    main :: IO ()
+    main = playIO (InWindow "Pacman" (600, 600) (0, 0)) -- Or FullScreen
+                  black            -- Background color
+                  30               -- Frames per second
+                  initialGameState -- Initial state
+                  view             -- View function
+                  input            -- Event function
+                  step             -- Step function
+
